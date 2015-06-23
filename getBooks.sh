062ee7b74bf4ec73f4c987a8a29584283f75307e
Damien Cassou <damien@cassou.me>
2015-06-23 15:35:55 +0200

    Remove useless commands

--- getBooks.sh
+++ getBooks.sh
@@ -13,11 +13,7 @@ wget http://gforge.inria.fr/frs/download.php/28665/PharoParLExemple-20-06-2011.p
 wget http://pharobyexample.org/es/PBE1-sp.pdf -O PharoByExample/PharoByExample-2013-SP.pdf
 wget https://4310b1a9-a-c5ded419-s-sites.googlegroups.com/a/smalltalk-users.jp/home/Home/docs/PBE1-ja.pdf?attachauth=ANoY7cpbYUFdHbZioU7tKMNbK3NFOyyRvSKNdXCKiQbrzJ5ndbYZHfRRBdc5vHz35fjI9LFfKmZ8EWYoMBiY0u1bpeJbQMoVFtDS2OF519_L5gGMPOxo05XsvAUSmGjvnzOQC-xP581mqUuGSeavMsSZoEUY9hh3MBWFNbe_e9Lr596Jb8wMfIO_Z8AO0Wd6XOcXWU_FVZTKpWbHS_f6UyPzhTJbzihTqF90PRqxlBpL0f_zUYc1ZaY%3D&attredirects=0 -O PharoByExample/PharoByExample-2013-JA.pdf
 
-
-cd StarterKit/Books/
 wget http://pharobooks.gforge.inria.fr/PharoByExampleTwo-Eng/latest/PBE2.pdf -O DeepIntoPharo/DeepIntoPharo-2013-EN.pdf
-
-cd StarterKit/Books/
 
 wget https://github.com/SquareBracketAssociates/NumericalMethods/releases/download/snapshot-2015-01-26/NumericalMethods-jan2015.pdf -O NumericalMethods/NumericalMethods-2015-EN.pdf
 
