f5eab9e61398787049a7f5b3d6346c73884ea526
Damien Cassou <damien@cassou.me>
2015-08-28 17:50:35 +0200

    Use capitals everywhere

--- getWebsites.sh
+++ getWebsites.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mkdir -p StarterKit/websites
-cd StarterKit/websites
+mkdir -p StarterKit/Websites
+cd StarterKit/Websites
 
 wget --adjust-extension --recursive --level=5 --convert-links --page-requisites pharo.org
