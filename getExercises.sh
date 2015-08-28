664559b89689cef77b2d2cf41d6bcb2ac99bc66f
Damien Cassou <damien@cassou.me>
2015-08-28 17:22:31 +0200

    Script to download the exercises

--- getExercises.sh
+++ getExercises.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-mkdir Exercises
+mkdir -p StarterKit
 
+cd StarterKit
 
+wget --adjust-extension --recursive --level=2 --convert-links --no-parent --cut-dirs=1 --no-host-directories http://sdmeta.gforge.inria.fr/StarterKitCache/Exercises/
 
+cd Exercises
+find . -name 'index.html*' -exec rm -f '{}' \;
 
+cd ../..
