0971b2cb9cdb82fc237b6d10ef320dcf16d38ab8
Damien Cassou <damien@cassou.me>
2015-08-31 13:39:59 +0200

    Add PBE image

--- getImages.sh
+++ getImages.sh
@@ -5,3 +5,4 @@ mkdir -p StarterKit/PharoImages
 cd StarterKit/PharoImages
 
 wget https://ci.inria.fr/pharo-contribution/job/Bootstrap/PHARO=40,VERSION=stable,VM=vm/lastSuccessfulBuild/artifact/Bootstrap.zip
+wget http://pharobyexample.org/image/PBE-OneClick-1.1.app.zip
