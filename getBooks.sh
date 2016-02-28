70888225242c13343ee4b19d0fd7f32b2135a028
StephaneDucasse <stephane.ducasse@inria.fr>
2016-02-28 15:44:33 +0100

    adressing some problems with slides and books.

--- getBooks.sh
+++ getBooks.sh
@@ -10,15 +10,15 @@ mkdir -p StarterKit/Books/NumericalMethods
 
 cd StarterKit/Books/
 
-wget http://pharobyexample.org/image/PBE-OneClick-1.1.app.zip -O 01-PharoByExample/PBE-OneClick-1.1.app.zip
-wget http://pharobyexample.org/versions/PBE1-2009-10-28.pdf -O 01-PharoByExample/PharoByExample-2009-EN.pdf
+wget http://gforge.inria.fr/frs/download.php/31048/PBE-OneClick-1.1.app.zip -O 01-PharoByExample/PBE-OneClick-1.1.app.zip
+wget http://gforge.inria.fr/frs/download.php/25599/PBE1-2009-10-28.pdf -O 01-PharoByExample/PharoByExample-2009-EN.pdf
 wget http://gforge.inria.fr/frs/download.php/28665/PharoParLExemple-20-06-2011.pdf -O 01-PharoByExample/PharoByExample-2011-FR.pdf
 wget http://pharobyexample.org/es/PBE1-sp.pdf -O 01-PharoByExample/PharoByExample-2013-SP.pdf
-wget 'https://4310b1a9-a-c5ded419-s-sites.googlegroups.com/a/smalltalk-users.jp/home/Home/docs/PBE1-ja.pdf?attachauth=ANoY7cpbYUFdHbZioU7tKMNbK3NFOyyRvSKNdXCKiQbrzJ5ndbYZHfRRBdc5vHz35fjI9LFfKmZ8EWYoMBiY0u1bpeJbQMoVFtDS2OF519_L5gGMPOxo05XsvAUSmGjvnzOQC-xP581mqUuGSeavMsSZoEUY9hh3MBWFNbe_e9Lr596Jb8wMfIO_Z8AO0Wd6XOcXWU_FVZTKpWbHS_f6UyPzhTJbzihTqF90PRqxlBpL0f_zUYc1ZaY%3D&attredirects=0' -O 01-PharoByExample/PharoByExample-2013-JA.pdf
+wget https://gforge.inria.fr/frs/download.php/32414/PBE1-ja.pdf -O 01-PharoByExample/PharoByExample-2013-JA.pdf
 
 wget http://pharobooks.gforge.inria.fr/PharoByExampleTwo-Eng/latest/PBE2.pdf -O 02-DeepIntoPharo/DeepIntoPharo-2013-EN.pdf
 
-wget https://ci.inria.fr/pharo-contribution/job/EnterprisePharoBook/lastSuccessfulBuild/artifact/book-result/EnterprisePharo-A4.pdf -O 03-EnterprisePharo/EnterprisePharo-A4.pdf
+wget https://ci.inria.fr/pharo-contribution/job/EnterprisePharoBook/lastSuccessfulBuild/artifact/book-result/EnterprisePharo.pdf -O 03-EnterprisePharo/EnterprisePharo.pdf
 
 wget https://github.com/SquareBracketAssociates/NumericalMethods/releases/download/snapshot-2015-01-26/NumericalMethods-jan2015.pdf -O NumericalMethods/NumericalMethods-2015-EN.pdf
 
--- getSlides.sh
+++ getSlides.sh
@@ -10,13 +10,7 @@ cd StarterKit
 tempzip="$(mktemp imageXXXXX.zip)"
 trap "rm '$tempzip'" EXIT
 
-wget 'https://ci.inria.fr/pharo-contribution/job/PharoMooc/lastSuccessfulBuild/artifact/book-result/Slides/4-Done/*zip*/4-Done.zip' -O $tempzip
+wget 'https://ci.inria.fr/pharo-contribution/job/PharoMooc/lastSuccessfulBuild/artifact/book-result/Slides/*zip*/Slides.zip' -O $tempzip
 unzip $tempzip
-mv 4-Done Slides
 
-find . \( -name '*.html' -or -name '*.tex' \) -exec rm -f '{}' \;
-cd Slides
-wget https://github.com/SquareBracketAssociates/PharoMooc/raw/master/Slides/4-Done/Intro-ImmersionInObjectLand.pdf
-wget https://github.com/SquareBracketAssociates/PharoMooc/raw/master/Slides/4-Done/2015-Seaside-InANutshell.pdf
-wget https://github.com/SquareBracketAssociates/PharoMooc/raw/master/Slides/4-Done/README.txt
 cd ..
