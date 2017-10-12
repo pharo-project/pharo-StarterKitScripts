de1a271c333ab0eb6a12a3bd8b1013998eda9661
StephaneDucasse <stephane.ducasse@inria.fr>
2017-10-12 18:34:09 +0200

    updated books

--- getBooks.sh
+++ getBooks.sh
@@ -1,25 +1,70 @@
 #!/usr/bin/env bash
-
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-mkdir -p StarterKit/Books/01-PharoByExample
-mkdir -p StarterKit/Books/02-DeepIntoPharo
-mkdir -p StarterKit/Books/03-EnterprisePharo
-mkdir -p StarterKit/Books/NumericalMethods
+mkdir -p StarterKit/BooksAndBooklets
+cd StarterKit/BooksAndBooklets
 
-cd StarterKit/Books/
+mkdir -p Spec
+cd Spec
+wget http://files.pharo.org/books-pdfs/spec/2017-01-23-SpecBooklet.pdf
+cd ../
 
-wget http://gforge.inria.fr/frs/download.php/31048/PBE-OneClick-1.1.app.zip -O 01-PharoByExample/PBE-OneClick-1.1.app.zip
-wget http://gforge.inria.fr/frs/download.php/25599/PBE1-2009-10-28.pdf -O 01-PharoByExample/PharoByExample-2009-EN.pdf
-wget http://gforge.inria.fr/frs/download.php/28665/PharoParLExemple-20-06-2011.pdf -O 01-PharoByExample/PharoByExample-2011-FR.pdf
-wget http://pharobyexample.org/es/PBE1-sp.pdf -O 01-PharoByExample/PharoByExample-2013-SP.pdf
-wget https://gforge.inria.fr/frs/download.php/32414/PBE1-ja.pdf -O 01-PharoByExample/PharoByExample-2013-JA.pdf
+mkdir -p LearningOOP
+cd LearningOOP
+wget http://files.pharo.org/books-pdfs/learning-oop/2017-09-29-LearningOOP.pdf
+cd ../
 
-wget http://pharobooks.gforge.inria.fr/PharoByExampleTwo-Eng/latest/PBE2.pdf -O 02-DeepIntoPharo/DeepIntoPharo-2013-EN.pdf
+mkdir -p DeepIntoPharo
+cd DeepIntoPharo
+wget http://files.pharo.org/books-pdfs/deep-into-pharo/2013-DeepIntoPharo-EN.pdf
+cd ../
 
-wget https://ci.inria.fr/pharo-contribution/job/EnterprisePharoBook/lastSuccessfulBuild/artifact/book-result/EnterprisePharo.pdf -O 03-EnterprisePharo/EnterprisePharo.pdf
+mkdir -p EnterprisePharo
+cd EnterprisePharo
+wget http://files.pharo.org/books-pdfs/entreprise-pharo/2016-10-06-EnterprisePharo.pdf
+cd ../
 
-wget https://github.com/SquareBracketAssociates/NumericalMethods/releases/download/snapshot-2015-01-26/NumericalMethods-jan2015.pdf -O NumericalMethods/NumericalMethods-2015-EN.pdf
+mkdir -p PharoByExample50
+cd PharoByExample50
+wget http://files.pharo.org/books-pdfs/updated-pharo-by-example/2017-01-14-UpdatedPharoByExample.pdf
+cd ../
 
-cp ../../README-books.txt README.txt
+mkdir -p NumericalMethods
+cd NumericalMethods
+wget https://github.com/SquareBracketAssociates/NumericalMethods/releases/download/snapshot-2015-01-26/NumericalMethods-jan2015.pdf -O NumericalMethods-2015-EN.pdf
+cd ../
+
+mkdir -p Booklet-Voyage
+cd Booklet-Voyage
+wget http://files.pharo.org/books-pdfs/booklet-Scraping/2017-09-29-scrapingbook.pdf
+cd ../
+
+mkdir -p Booklet-Smacc
+cd Booklet-Smacc
+wget http://files.pharo.org/books-pdfs/booklet-Smacc/2017-05-05-Smacc-Spiral.pdf
+cd ../
+
+mkdir -p Booklet-Glorp
+cd Booklet-Smacc
+wget http://files.pharo.org/books-pdfs/booklet-Glorp/2017-05-02-Glorp-Spiral.pdf
+cd ../
+
+mkdir -p Booklet-Voyage
+cd Booklet-Voyage
+wget http://files.pharo.org/books-pdfs/booklet-Voyage/2017-05-14-Voyage-Spiral.pdf
+cd ../
+
+mkdir -p Booklet-Bloc
+cd Booklet-Bloc
+wget http://files.pharo.org/books-pdfs/booklet-Bloc/BLOCDRAFT.pdf
+cd ../
+
+mkdir -p TinyBlog
+cd TinyBlog
+wget http://files.pharo.org/books-pdfs/tinyblog-tutorial/2016-02-18-tinyblog-tutorial.pdf
+cd ../
+
+
+cd ../
+cp README-books.txt Starter/BooksAndBooklets/README.txt
