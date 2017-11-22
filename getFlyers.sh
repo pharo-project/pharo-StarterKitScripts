f0530a01f51bd64def630466c8fc9c527bd21c0c
StephaneDucasse <stephane.ducasse@inria.fr>
2017-11-22 19:45:51 +0100

    flyers ok

--- getFlyers.sh
+++ getFlyers.sh
@@ -6,6 +6,10 @@ set -e
 mkdir -p StarterKit
 
 cd StarterKit
+mkdir -p CheatSheet
+cd CheatSheet
 
-wget --adjust-extension --recursive --level=1 --convert-links --no-parent --cut-dirs=1 --no-host-directories http://sdmeta.gforge.inria.fr/StarterKitCache/Flyers/
-find Flyers -name 'index.html*' -exec rm -f '{}' \;
+#wget --adjust-extension --recursive --level=1 --convert-links --no-parent --cut-dirs=1 --no-host-directories #http://sdmeta.gforge.inria.fr/StarterKitCache/Flyers/
+#find Flyers -name 'index.html*' -exec rm -f '{}' \;
+
+wget http://files.pharo.org/media/pharoCheatSheet.pdf
